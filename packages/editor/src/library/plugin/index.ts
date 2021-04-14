768144f8183a97a95353c134d12ad2bbaa278988
boen <1997@boenfu.cn>
2021-04-14 16:47:46 +0800

    Update types

--- packages/editor/src/library/plugin/index.ts
+++ packages/editor/src/library/plugin/index.ts
@@ -1,46 +1,51 @@
-import {Node} from '@magicflow/core';
+import {Node, NodeType} from '@magicflow/core';
 import {ProcedureTreeNode} from '@magicflow/procedure';
 import {ComponentType, ReactNode} from 'react';
 
 import {ProcedureEditor} from '../procedure-editor';
 
-export interface PluginComponentProps {
+export interface PluginComponentProps<TType extends NodeType = NodeType> {
   editor: ProcedureEditor;
-  node: ProcedureTreeNode;
+  node: Extract<ProcedureTreeNode, {type: TType}>;
   prevChildren?: ReactNode;
 }
 
-export type PluginComponent<TProps = {}> = ComponentType<
-  PluginComponentProps & TProps
->;
+export type PluginComponent<
+  TType extends NodeType,
+  TProps = {}
+> = ComponentType<PluginComponentProps<TType> & TProps>;
 
-export type PluginConfigComponent<TProps = {}> = PluginComponent<
+export type PluginConfigComponent<
+  TType extends NodeType = NodeType,
+  TProps = {}
+> = PluginComponent<
+  TType,
   TProps & {
-    value: Node;
-    onChange(node: Node): void;
+    value: Extract<Node, {type: TType}>;
+    onChange(node: Extract<Node, {type: TType}>): void;
   }
 >;
 
 export interface IPlugin<TConfigExtraProps = {}> {
   name: string;
   singleNode?: {
-    before?: PluginComponent;
-    after?: PluginComponent;
-    headLeft?: PluginComponent;
-    headRight?: PluginComponent;
+    before?: PluginComponent<'singleNode'>;
+    after?: PluginComponent<'singleNode'>;
+    headLeft?: PluginComponent<'singleNode'>;
+    headRight?: PluginComponent<'singleNode'>;
     /**
      * default `flex-direction: column`
      */
-    body?: PluginComponent;
+    body?: PluginComponent<'singleNode'>;
     /**
      * default `flex-direction: row`
      */
-    footer?: PluginComponent;
-    config?: PluginConfigComponent<TConfigExtraProps>;
+    footer?: PluginComponent<'singleNode'>;
+    config?: PluginConfigComponent<'singleNode', TConfigExtraProps>;
   };
   branchesNode?: {
-    before?: PluginComponent;
-    after?: PluginComponent;
-    config?: PluginConfigComponent<TConfigExtraProps>;
+    before?: PluginComponent<'branchesNode'>;
+    after?: PluginComponent<'branchesNode'>;
+    config?: PluginConfigComponent<'branchesNode', TConfigExtraProps>;
   };
 }
